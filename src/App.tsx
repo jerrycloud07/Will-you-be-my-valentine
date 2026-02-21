7896b6d743fbfb819339aa62f64efdf9c829813d
jerrycloud07 <105712948+jerrycloud07@users.noreply.github.com>
2026-02-22 01:37:49 +0530

    Refactor App component for mouse interaction
    Refactor App component to handle mouse movement and create hearts on mousemove. Removed old state management and timers for showing messages.

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,150 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+// import { useEffect, useState } from 'react'
+// import './App.css'
 
-const phrases = [
-  "No",
-  "Are you sure?",
-  "Really sure?",
-  "Pookie pleasee",
-  "I'll buy you food <3",
-  "and flowers...",
-  "and cake...",
-  "Okay... You sure?",
-  "Sure sure?",
-  "Sure sure sure?",
-  "Sure sure sure sure?",
-  "Sure sure sure sure sure?",
-  "Sure sure sure sure sure sure?",
-  "Sure sure sure sure sure sure sure?",
-  "Change of heart yet?"
-]
+// const phrases = [
+//   "No",
+//   "Are you sure?",
+//   "Really sure?",
+//   "Pookie pleasee",
+//   "I'll buy you food <3",
+//   "and flowers...",
+//   "and cake...",
+//   "Okay... You sure?",
+//   "Sure sure?",
+//   "Sure sure sure?",
+//   "Sure sure sure sure?",
+//   "Sure sure sure sure sure?",
+//   "Sure sure sure sure sure sure?",
+//   "Sure sure sure sure sure sure sure?",
+//   "Change of heart yet?"
+// ]
+
+// function App() {
+//   const [noCount, setNoCount] = useState(0);
+//   const [yesPressed, setYesPressed]= useState(false);
+//   const yesButtonSize = noCount * 20 + 16;
+//   const noButtonSize = 16;
+
+//   const [showHey, setShowHey] = useState(true);
+//   const [showValentines, setShowValentines] = useState(false);
+//   const [showSo, setShowSo] = useState(false);
+
+//   useEffect(() => {
+//     const heyTimer = setTimeout(() => {
+//       setShowHey(false);
+//       setShowValentines(true);
+//     }, 2100);
+
+//     const valentinesTimer = setTimeout(() => {
+//       setShowValentines(false);
+//       setShowSo(true);
+//     }, 4900);
+
+//     const soTimer = setTimeout(() => {
+//       setShowSo(false);
+//     }, 6600);
+
+//     return () => {
+//       clearTimeout(heyTimer);
+//       clearTimeout(valentinesTimer);
+//       clearTimeout(soTimer);
+//     };
+//   }, []);
+
+//   function handleNoClick() {
+//     setNoCount(noCount + 1);
+//   }
+
+//   function getNoButtonText() {
+//     return phrases[Math.min(noCount, phrases.length - 1)];
+//   }
+  
+//   return (
+//     <div className='valentine-container items-center'>
+//       {showHey && 
+//       <div className='fade-in-out center-content zoom-in slidein'>
+//         <div className='text text-shadow pb-2' style={{fontSize: 50}}><strong>Hey you</strong>...</div>
+//         <img alt='cat happy' style={{height:'200px'}} src='https://media.tenor.com/8tgG_KyJqqwAAAAi/happy-happy-happy-happy.gif'/>
+//       </div>
+//       }
+//       {showValentines && (
+//       <div className='fade-in-out center-content zoom-in text-shadow'>
+//         <div className='text pb-8' style={{fontSize: 40}}><strong>Valentine's is around the corner ;)</strong></div>
+//         <img alt='cat sexy' style={{height:'150px'}} src='https://media.tenor.com/mtX466Hr5sYAAAAi/cat-kitty.gif'/>
+//       </div>
+//       )}
+//       {showSo && 
+//       <div className='fade-in-out center-content zoom-in text-shadow'>
+//         <div className='text rotate' style={{fontSize: 120}}><strong>SO..</strong></div>
+//         <img alt='cat blush' style={{height:'150px'}} src='https://media.tenor.com/8LW89Wyqp24AAAAj/syz-blush-syz.gif'/>
+//       </div>
+//       }
+//       {!showHey && !showValentines && !showSo && (
+//         <div>
+//           { yesPressed ? (
+//             <div className='fade-in-out center-content zoom-in'>
+//             <img alt='love hug' style={{height:'250px'}} src='https://media.tenor.com/8DHkW1uWDHsAAAAj/milk-and-mocha.gif'/>
+//             <div className='text' style={{fontSize: 40}}> <strong>YAYE!!!</strong> 🥰🤗💐😭😚💝</div>
+//             </div>
+//           ) : (
+//             <div className='center-content zoom-in'>
+//             <img className='zoom-in' style={{height:'200px'}} alt='bears with hearts' src='https://media.tenor.com/hpgCwxnBaK8AAAAi/milk-and-mocha.gif' />
+//             <div className='text text-color-black' style={{fontSize: 40}}> Will you be my <strong>Valentine?</strong> 🥺</div>
+//             <div>
+//               <button className='yes-button yes-button:hover' onClick={() => setYesPressed(true)} style={{fontSize: yesButtonSize}}>Yes</button>
+//               <button className='no-button no-button:hover' onClick={handleNoClick} style={{fontSize: noButtonSize}}>{getNoButtonText()}</button>
+//             </div>
+//             </div> 
+//           )}
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import { useEffect } from "react";
 
 function App() {
-  const [noCount, setNoCount] = useState(0);
-  const [yesPressed, setYesPressed]= useState(false);
-  const yesButtonSize = noCount * 20 + 16;
-  const noButtonSize = 16;
-
-  const [showHey, setShowHey] = useState(true);
-  const [showValentines, setShowValentines] = useState(false);
-  const [showSo, setShowSo] = useState(false);
 
   useEffect(() => {
-    const heyTimer = setTimeout(() => {
-      setShowHey(false);
-      setShowValentines(true);
-    }, 2100);
 
-    const valentinesTimer = setTimeout(() => {
-      setShowValentines(false);
-      setShowSo(true);
-    }, 4900);
+    const moveHandler = (e: MouseEvent) => {
 
-    const soTimer = setTimeout(() => {
-      setShowSo(false);
-    }, 6600);
+      const heart = document.createElement("span");
+
+      heart.style.position = "absolute";
+      heart.style.left = e.clientX + 20 + "px";
+      heart.style.top = e.clientY + 20 + "px";
+
+      let size = Math.random() * 60 + 20;
+      heart.style.width = size + "px";
+      heart.style.height = size + "px";
+
+      heart.style.background = "red";
+      heart.style.borderRadius = "50%";
+
+      document.body.appendChild(heart);
+
+      setTimeout(() => {
+        heart.remove();
+      }, 300);
+    };
+
+    document.addEventListener("mousemove", moveHandler);
 
     return () => {
-      clearTimeout(heyTimer);
-      clearTimeout(valentinesTimer);
-      clearTimeout(soTimer);
+      document.removeEventListener("mousemove", moveHandler);
     };
+
   }, []);
 
-  function handleNoClick() {
-    setNoCount(noCount + 1);
-  }
-
-  function getNoButtonText() {
-    return phrases[Math.min(noCount, phrases.length - 1)];
-  }
-  
   return (
-    <div className='valentine-container items-center'>
-      {showHey && 
-      <div className='fade-in-out center-content zoom-in slidein'>
-        <div className='text text-shadow pb-2' style={{fontSize: 50}}><strong>Hey you</strong>...</div>
-        <img alt='cat happy' style={{height:'200px'}} src='https://media.tenor.com/8tgG_KyJqqwAAAAi/happy-happy-happy-happy.gif'/>
-      </div>
-      }
-      {showValentines && (
-      <div className='fade-in-out center-content zoom-in text-shadow'>
-        <div className='text pb-8' style={{fontSize: 40}}><strong>Valentine's is around the corner ;)</strong></div>
-        <img alt='cat sexy' style={{height:'150px'}} src='https://media.tenor.com/mtX466Hr5sYAAAAi/cat-kitty.gif'/>
-      </div>
-      )}
-      {showSo && 
-      <div className='fade-in-out center-content zoom-in text-shadow'>
-        <div className='text rotate' style={{fontSize: 120}}><strong>SO..</strong></div>
-        <img alt='cat blush' style={{height:'150px'}} src='https://media.tenor.com/8LW89Wyqp24AAAAj/syz-blush-syz.gif'/>
-      </div>
-      }
-      {!showHey && !showValentines && !showSo && (
-        <div>
-          { yesPressed ? (
-            <div className='fade-in-out center-content zoom-in'>
-            <img alt='love hug' style={{height:'250px'}} src='https://media.tenor.com/8DHkW1uWDHsAAAAj/milk-and-mocha.gif'/>
-            <div className='text' style={{fontSize: 40}}> <strong>YAYE!!!</strong> 🥰🤗💐😭😚💝</div>
-            </div>
-          ) : (
-            <div className='center-content zoom-in'>
-            <img className='zoom-in' style={{height:'200px'}} alt='bears with hearts' src='https://media.tenor.com/hpgCwxnBaK8AAAAi/milk-and-mocha.gif' />
-            <div className='text text-color-black' style={{fontSize: 40}}> Will you be my <strong>Valentine?</strong> 🥺</div>
-            <div>
-              <button className='yes-button yes-button:hover' onClick={() => setYesPressed(true)} style={{fontSize: yesButtonSize}}>Yes</button>
-              <button className='no-button no-button:hover' onClick={handleNoClick} style={{fontSize: noButtonSize}}>{getNoButtonText()}</button>
-            </div>
-            </div> 
-          )}
-        </div>
-      )}
-    </div>
-  )
+    <h1 style={{ textAlign: "center" }}>
+      ❤️ Be My Valentine ❤️
+    </h1>
+  );
 }
 
-export default App
+export default App;
